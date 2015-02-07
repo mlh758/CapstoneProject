6a9ba47c67676b9af85be254da6d027301d4a71b
Michael <osiris2918@gmail.com>
2015-02-07 08:39:10 -0600

    Added LINIQ Queries
    Provided by Jonathan

---
+++ On-Call Assistant Project/On-Call Assistant/On-Call Assistant/Group Code/LinqQueries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using On_Call_Assistant.DAL;
+using On_Call_Assistant.Models;
+using System.Web.ModelBinding;
+
+
+namespace On_Call_Assistant.Group_Code
+{
+
+    /*This is a static class, so you don't need to create an instance of the class.
+     *To call one of the methods, call the class itself and invoke the method.
+     *Be sure to pass in the db context.
+     *Example: to get a list of all employees:
+     *List<Employee> emps = LinqQueries.GetEmployees(db);    
+     */
+    static class LinqQueries
+    {
+        public static List<Employee> GetEmployees(OnCallContext db)
+        {
+            return db.employees.ToList();
+        }
+        
+        public static void SaveRotations(OnCallContext db, List<OnCallRotation> rotations)
+        {
+            foreach (OnCallRotation r in rotations)
+            { 
+                db.onCallRotations.Add(r);              
+            }
+            db.SaveChanges();
+        }
+    
+    }
+}

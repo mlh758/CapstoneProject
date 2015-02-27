e9533392c9c210d018c8547603e595fe43b6f346
William Freeman <wjfkc2@mail.umkc.edu>
2015-02-27 12:40:43 -0600

    Git missed some files to commit

---
+++ On-Call Assistant Project/On-Call Assistant/On-Call Assistant/Migrations/201502271810172_EmployeeRotationList.cs
@@ -0,0 +1,16 @@
+namespace On_Call_Assistant.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeRotationList : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

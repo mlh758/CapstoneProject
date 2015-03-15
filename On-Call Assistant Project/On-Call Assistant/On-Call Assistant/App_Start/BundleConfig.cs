429dc8c471db9947ff938dd820b8206e68722384
William Freeman <wjfkc2@mail.umkc.edu>
2015-03-14 20:05:38 -0500

    DatePicker Support For Multiple Browsers Added
    Only tested with Chrome and IE, need to test with FireFox, and any other
    browser we have.

--- On-Call Assistant Project/On-Call Assistant/On-Call Assistant/App_Start/BundleConfig.cs
+++ On-Call Assistant Project/On-Call Assistant/On-Call Assistant/App_Start/BundleConfig.cs
@@ -26,6 +26,15 @@ namespace On_Call_Assistant
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            //Added
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                      "~/Scripts/jquery-ui-{version}.js",
+                      "~/Scripts/jquery-ui.unobtrusive-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                       "~/Content/themes/base/jquery.ui.core.css",
+                       "~/Content/themes/base/jquery.ui.datepicker.css",
+                       "~/Content/themes/base/jquery.ui.theme.css")); 
         }
     }
 }
